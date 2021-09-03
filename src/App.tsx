88169be605595919f1fb4d9feb28e1eb9872073d
gretten <any@anykey.com>
2021-09-03 07:45:53 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
 
 function App() {
+
+  const [books, setBooks] = React.useState([]);
+
+  const getAddress = (parameter: string): string => {
+
+    return `https://www.googleapis.com/books/v1/volumes?q=${parameter}&key=AIzaSyCBVroRsJ0yUbiLU8r2pT4dzRdt6oFbUOw`
+  }
+
+  React.useEffect(
+    () => {
+
+      fetch(getAddress('horror'))
+        .then(res => res.json())
+        .then(data => console.log(data))
+
+    }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+
     </div>
   );
 }
